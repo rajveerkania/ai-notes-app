fc152f11a197751f5c8605ef7ce60819a2121b01
Rajveer Kania <rajveerkania02@gmail.com>
2025-04-23 19:47:22 +0530

    Cusotm pnpm setup

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,2 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: undefined, 
-
-  images: {
-    domains: ["your-supabase-domain.supabase.co"], 
-  },
-
-  reactStrictMode: true
-};
-
-export default nextConfig;
+module.exports = {
+}
