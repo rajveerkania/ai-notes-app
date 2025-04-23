044ab63487a947f563074c731d78a183dcf9f68d
Rajveer Kania <rajveerkania02@gmail.com>
2025-04-23 17:56:15 +0530

    index.js pnpm installed

--- netlify/plugins/pnpm-install/index.js
+++ netlify/plugins/pnpm-install/index.js
@@ -1,11 +1,25 @@
+const { execSync } = require('child_process')
+
 module.exports = {
-    onPreBuild: async ({ utils }) => {
+  onPreBuild: async ({ utils }) => {
+    try {
+      execSync('pnpm --version', { stdio: 'ignore' })
+      console.log('pnpm is already installed')
+    } catch {
       try {
         await utils.run.command('npm install -g pnpm')
-        await utils.run.command('pnpm install')
-        console.log('Installed dependencies with pnpm')
-      } catch (error) {
-        utils.build.failBuild('Failed to install dependencies with pnpm', { error })
+        console.log('Installed pnpm globally')
+      } catch (installError) {
+        utils.build.failBuild('Failed to install pnpm globally', { error: installError })
+        return
       }
-    },
-  }
+    }
+
+    try {
+      await utils.run.command('pnpm install')
+      console.log('Installed dependencies with pnpm')
+    } catch (installError) {
+      utils.build.failBuild('Failed to install dependencies with pnpm', { error: installError })
+    }
+  },
+}
